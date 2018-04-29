1480bb1ad5214e5d7a0656a7435e4d900ea48ebc
Alex Gravenor <blazingkin@gmail.com>
2018-04-28 17:12:20 -0700

    Rhythm

--- sketch.js
+++ sketch.js
@@ -1,46 +1,99 @@
+function preload(){
+  soundFormats('wav', 'ogg');
+  snare = loadSound('snare.ogg');
+}
+
 function setup() {
   createCanvas(800, 800);
   stroke(0,0,0,0);
-  background(0);
-  oscillator = new p5.Oscillator();
-  oscillator.setType('sine');
+  snare.setVolume(0.1);
+  for (var i = 0; i <= width / blockWidth; i++){
+    addStreamElement();
+  }
 }
 
-let button_one = "a"
-let button_two = "s"
+var snare = null;
+
+function mod(a, n) {
+  return a - (n * Math.floor(a/n));
+}
+
+function addStreamElement(){
+  if (streams.length == 0){
+    streams.push(Math.floor(Math.random() * numStreams));
+  }else{
+    var newLevel = streams[streams.length - 1];
+    if (Math.random() >= 0.5){
+      newLevel += 1;
+    }else{
+      newLevel -= 1;
+    }
+    newLevel = mod(newLevel, numStreams);
+    console.log(newLevel);
+    streams.push(newLevel);
+  }
+}
+
+let buttonOne = "a"
+let buttonTwo = "s"
 var oscillator;
 
-function onButtonOne(){
+var numStreams = 4;
+var onStream = 0;
+var streams = [];
 
+function onButtonOne(){
+  onStream = mod((onStream - 1), numStreams);
 }
 
 function onButtonTwo(){
-
+  onStream = mod((onStream + 1), numStreams);
 }
 
 /* 120 bpm */
 let msDelay = 500;
-
-var recSize = 1;
 function onBeat() {
-  recSize+= 10;
+  if (snare != null){
+    snare.play();
+  }
+  streams = streams.slice(1, streams.length);
+  addStreamElement();
+  
   setTimeout(onBeat, msDelay);
 }
 setTimeout(onBeat, msDelay);
 
+
+var blockWidth =  100;
 function draw() {
   // put drawing code here
+  clear();
+  background(0);
   fill('red');
-  rect((width-recSize)/2,(height-recSize)/2,recSize,recSize);
+  drawPlayer();
+  fill('blue')
+  drawBeats();
+}
+
+function drawPlayer(){
+  let pHeight = height / numStreams;
+  rect(0, pHeight * onStream, blockWidth, pHeight);
+}
+
+function drawBeats(){
+  let bHeight = height / numStreams;
+  for (var i = 0; i < streams.length; i++){
+    rect((1 + i) * blockWidth, streams[i] * bHeight, blockWidth, bHeight);
+  }
 }
 
 document.onkeypress = function(evt) {
   evt = evt || window.event;
   var charCode = evt.keyCode || evt.which;
   var charStr = String.fromCharCode(charCode);
-  if (charStr == button_one){
+  if (charStr == buttonOne){
     onButtonOne();
-  }else if (charStr == button_two) {
+  }else if (charStr == buttonTwo) {
     onButtonTwo();
   }
 };
